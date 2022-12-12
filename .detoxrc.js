fe6bcf3b9505533e9e667bc9808aa26b9b965884
Austin Moore <austin.moore@fansunite.com>
2022-12-12 16:09:30 -0500

    update ios build command

--- .detoxrc.js
+++ .detoxrc.js
@@ -12,13 +12,13 @@ module.exports = {
   apps: {
     'ios.debug': {
       type: 'ios.app',
-      binaryPath: 'ios/build/Build/Products/Debug-iphonesimulator/YOUR_APP.app',
-      build: 'xcodebuild -workspace ios/YOUR_APP.xcworkspace -scheme YOUR_APP -configuration Debug -sdk iphonesimulator -derivedDataPath ios/build'
+      binaryPath: 'ios/build/Build/Products/Debug-iphonesimulator/MyTestProject.app',
+      build: 'xcodebuild -workspace ios/MyTestProject.xcworkspace -scheme MyTestProject -configuration Debug -sdk iphonesimulator -derivedDataPath ios/build'
     },
     'ios.release': {
       type: 'ios.app',
-      binaryPath: 'ios/build/Build/Products/Release-iphonesimulator/YOUR_APP.app',
-      build: 'xcodebuild -workspace ios/YOUR_APP.xcworkspace -scheme YOUR_APP -configuration Release -sdk iphonesimulator -derivedDataPath ios/build'
+      binaryPath: 'ios/build/Build/Products/Release-iphonesimulator/MyTestProject.app',
+      build: 'xcodebuild -workspace ios/MyTestProject.xcworkspace -scheme MyTestProject -configuration Release -sdk iphonesimulator -derivedDataPath ios/build'
     },
     'android.debug': {
       type: 'android.apk',
